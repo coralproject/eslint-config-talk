d15b851a6831b6fa733057c0676c1017dd20ecd7
Chi Vinh Le <vinh@wikiwi.io>
2017-09-26 19:33:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ESLint-config-Talk
+
+Linting preset for developing talk plugins.
+
+## Preset for server side code
+
+```json
+  {
+    "extends": "@coralproject/eslint-config-talk"
+  }
+```
+
+## Preset for client side code
+
+```json
+  {
+    "extends": "@coralproject/eslint-config-talk/client"
+  }
+```
+
---
+++ client.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "mocha": true
+  },
+  "extends": "./server.js",
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    }
+  },
+  "parser": "babel-eslint",
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+
+    "react/display-name": "off",
+
+    // We'll keeping using this, as we'll switch over to Preact and we don't
+    // need to worry about deprecation.
+    "react/no-find-dom-node": "off",
+
+    // TODO: Use default after fixing the errors.
+    "react/prop-types": "warn",
+
+    "no-console": ["warn", { "allow": ["warn", "error"] }]
+  }
+};
