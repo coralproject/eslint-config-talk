b5aa52a2c753513737a58e5aa12929e423505bcb
Wyatt Johnson <wyattjoh@gmail.com>
2018-01-11 19:53:15 -0700

    prettier support

--- client.js
+++ client.js
@@ -1,40 +1,37 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "mocha": true,
-    "jest": true
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+    mocha: true,
+    jest: true,
   },
-  "extends": [
-    "./server.js",
-    "plugin:react/recommended",
-    "plugin:jest/recommended"
-  ],
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true
-    }
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+    },
   },
-  "parser": "babel-eslint",
-  "plugins": [
-    "jest",
-    "react"
+  plugins: ['jest', 'react'],
+  extends: [
+    './server.js',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
   ],
-  "rules": {
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
 
-    "react/display-name": "off",
+    'react/display-name': 'off',
 
     // We'll keeping using this, as we'll switch over to Preact and we don't
     // need to worry about deprecation.
-    "react/no-find-dom-node": "off",
+    'react/no-find-dom-node': 'off',
 
     // TODO: Use default after fixing the errors.
-    "react/prop-types": "warn",
+    'react/prop-types': 'warn',
 
-    "no-console": ["warn", { "allow": ["warn", "error"] }]
-  }
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
 };
